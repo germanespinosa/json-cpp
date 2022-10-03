f867beb37fe61b1cf3a7afbc7966c4c91c14a0ff
German Espinosa <germanespinosa@gmail.com>
2022-10-03 18:24:33 -0500

    added json_type

--- include/json_cpp/python/json_python.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <json_cpp/python/json_python_list.h>
-#include <json_cpp/python/json_python_value.h>
-#include <json_cpp/python/json_python_object.h>
--- src/python/json_python_list.cpp
+++
@@ -1,3 +0,0 @@
-#include <json_cpp/python/json_python_list.h>
-namespace json_cpp{
-}
