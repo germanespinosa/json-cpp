e58600bd0baf87199db17d0360eeb5c9c31fe3eb
German Espinosa <germanespinosa@gmail.com>
2022-10-23 14:15:06 -0600

    python new version

---
+++ python/json-cpp2/setup/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(name='json-cpp2',description='a better json library',author='german espinosa',author_email='germanespinosa@gmail.com',packages=['json_cpp2'],package_data={'': ['core.cpython-37m-x86_64-linux-gnu.so', 'libc.so.6', 'libgcc_s.so.1', 'libm.so.6', 'libstdc++.so.6']},license='MIT',version='0.0.62',zip_safe=False)
