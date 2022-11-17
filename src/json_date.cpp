3f53cad54f53e0a54c33d325b2f6c6d0a1086b6c
German Espinosa <germanespinosa@gmail.com>
2022-11-17 14:56:14 -0600

    added Json_date set_local_time_zone_offset

--- src/json_date.cpp
+++ src/json_date.cpp
@@ -54,16 +54,20 @@ namespace json_cpp{
     }
 
     std::string exec(const char* cmd) {
-        std::array<char, 128> buffer;
         std::string result;
-        std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-        if (!pipe) {
-            throw std::runtime_error("popen() failed!");
-        }
-        while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
-            result += buffer.data();
-        }
-        return result;
+        #ifdef __linux__
+            //linux code goes here
+            std::array<char, 128> buffer;
+            std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+            if (!pipe) {
+                throw std::runtime_error("popen() failed!");
+            }
+            while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+                result += buffer.data();
+            }
+            return result;
+        #elif _WIN32
+        #endif
     }
 
     void Json_date::set_local_time_zone_offset() {
